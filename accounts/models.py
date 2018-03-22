8256a60539dbad4329d93fdbd84f9f98318cf5b1
Yogi <patelsyogi@gmail.com>
2018-03-22 15:43:10 -0500

    created all the models/relations

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+#adjacency to built in Django user
+class IsInstructor(models.Model):
+    netID = models.TextField(primary_key=True) #django user has username instead of netID
+    is_instructor = models.BooleanField()
+
+
+#relationships
+class PartneredWith(models.Model):
+    netID_1 = models.TextField()
+    netID_2 = models.TextField()
+    recent_timestamp = models.DateTimeField(auto_now_add=True) #auto_now saves on all updates
+    number_interactions = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = (('netID_1', 'netID_2'),)
