dfc5e78b039c4007e085ed7eae8969e2f0357b7f
Yogi <patelsyogi@gmail.com>
2018-04-13 17:07:35 -0500

    created front end stuff for create sesh, jj merged

--- home/views.py
+++ home/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse #used for namespaces
 from django.http import HttpResponse
 from home.forms import *
 from datetime import datetime, timedelta
+import operator
 
 # Create your views here.
 
@@ -70,7 +71,7 @@ class HomeView(TemplateView):
                         ORDER BY    home_studysession.start_time", [str(request.user)])
 
         sessions_arr = cursor.fetchall()
-        print(sessions_arr)
+        #print(sessions_arr)
 
         for i in range(len(sessions)):
             sessions[i]['is_owner'] = 0
@@ -345,11 +346,115 @@ class NewSessionView(TemplateView):
         for i in range(len(middleman)):
             classes.append(middleman[i][0])
         class_dict = {}
+        #need to obtain weights for every class scenario
+        for curr_class in classes:
+            #obtain all the users and corresponding dates that current user has studied
+            cursor.execute("SELECT  s2.netID, home_studysession.date        \
+                            FROM    home_sessionhas s1,               \
+                                    home_sessionhas s2,               \
+                                    home_classofsession,             \
+                                    home_studysession           \
+                            WHERE   %s = s1.netID AND \
+                                    s1.seshID = home_classofsession.seshID  AND \
+                                    s1.seshID = home_studysession.seshID    AND \
+                                    home_classofsession.class_code = %s     AND \
+                                    s1.netID <> s2.netID    AND \
+                                    s1.seshID = s2.seshID", [str(request.user), str(curr_class)])
+
+            date_arr = cursor.fetchall()
+            print(date_arr)
+            exists_dict = {}
+            session_arr = []
+            for i in range(len(date_arr)):
+                if date_arr[i][0] not in exists_dict:
+                    if (datetime.now().date() - date_arr[i][1]).days >= 0:
+                        exists_dict[date_arr[i][0]] = len(session_arr)
+                        session_arr.append([])
+                        session_arr[len(session_arr)-1].append(date_arr[i][0])
+                        session_arr[len(session_arr)-1].append(1)
+                        session_arr[len(session_arr)-1].append(date_arr[i][1])
+                else:
+                    session_arr[exists_dict[date_arr[i][0]]][1] += 1
+                    if (datetime.now().date() - date_arr[i][1]).days >= 0 and (date_arr[i][1] - session_arr[exists_dict[date_arr[i][0]]][2]).days >= 0:
+                        session_arr[exists_dict[date_arr[i][0]]][2] = date_arr[i][1]
+            print(session_arr)
+            #aggregate the study sessions
+            user_dict = {}
+            #define weights
+            for i in range(len(session_arr)):
+                user_dict[session_arr[i][0]] = 4 + session_arr[i][1]
+                #apply decay if in past
+                delta = datetime.now().date() - session_arr[i][2]
+                if delta.days >= 0:
+                    user_dict[session_arr[i][0]] = max(0, user_dict[session_arr[i][0]] - (int(delta.days/14)))
+            #print(user_dict)
+
+            #MOM (Mate of a Mate) search. Go into current suggests and see if they have any strong relations
+            mate_dict = []
+            for j in user_dict.keys():
+                mate_dict.append({})
+                #go through each linked user and see who they have worked with and update the user dict
+                cursor.execute("SELECT  s2.netID, home_studysession.date        \
+                                FROM    home_sessionhas s1,               \
+                                        home_sessionhas s2,               \
+                                        home_classofsession,             \
+                                        home_studysession           \
+                                WHERE   %s = s1.netID AND \
+                                        s1.seshID = home_classofsession.seshID  AND \
+                                        s1.seshID = home_studysession.seshID    AND \
+                                        home_classofsession.class_code = %s     AND \
+                                        s1.netID <> s2.netID    AND \
+                                        s2.netID <> %s  AND \
+                                        s1.seshID = s2.seshID", [str(j), str(curr_class), str(request.user)])
+
+
+                date_arr = cursor.fetchall()
+                #print(date_arr)
+                exists_dict = {}
+                temp_arr = []
+                for i in range(len(date_arr)):
+                    if date_arr[i][0] not in exists_dict:
+                        if (datetime.now().date() - date_arr[i][1]).days >= 0:
+                            exists_dict[date_arr[i][0]] = len(temp_arr)
+                            temp_arr.append([])
+                            temp_arr[len(temp_arr)-1].append(date_arr[i][0])
+                            temp_arr[len(temp_arr)-1].append(1)
+                            temp_arr[len(temp_arr)-1].append(date_arr[i][1])
+                    else:
+                        temp_arr[exists_dict[date_arr[i][0]]][1] += 1
+                        if (datetime.now().date() - date_arr[i][1]).days >= 0 and (date_arr[i][1] - temp_arr[exists_dict[date_arr[i][0]]][2]).days >= 0:
+                            temp_arr[exists_dict[date_arr[i][0]]][2] = date_arr[i][1]
+
+                #aggregate the study sessions
+                for i in range(len(temp_arr)):
+                    mate_dict[len(mate_dict)-1][temp_arr[i][0]] = 4 + temp_arr[i][1]
+                    delta = datetime.now().date() - temp_arr[i][2]
+                    if delta.days >= 0:
+                        #initalize the dictionary
+                        mate_dict[len(mate_dict)-1][temp_arr[i][0]] = int(max(0, mate_dict[len(mate_dict)-1][temp_arr[i][0]] - (int(delta.days/14))))
+
+
+            for j in range(len(mate_dict)):
+                #using the values in this dictionary, store into user dict
+                for i in mate_dict[j].keys():
+                    if i not in user_dict.keys():
+                        user_dict[i] = int(float(mate_dict[j][i]*0.20) + 0.5)
+                    else:
+                        user_dict[i] = max(int(float(mate_dict[j][i]*0.20) + 0.5), user_dict[i])
+            #sort user_dict
+            sorted_x = sorted(user_dict.items(), key=operator.itemgetter(1), reverse = True)
+            class_dict[curr_class] = sorted_x
+
+        #debug print
+        for j in class_dict.keys():
+            print(j)
+            for i in range(len(class_dict[j])):
+                print(str(class_dict[j][i][1]) + " Weight for study mate " + str(class_dict[j][i][0]))
+
         cursor.close()
 
         form = NewSessionForm()
         args = {'form': form, 'classes': classes, 'class_dict': class_dict}
-        print(class_dict)
         return render(request, self.template_name, args)
 
     def post(self, request):
