9da602ddfe1c347f83e061812e9754b89bfc8d75
Yogi <patelsyogi@gmail.com>
2018-04-15 00:43:11 -0500

    dynamically added classes to profile page

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render, redirect
+from django.db import connection #sql
 from django.urls import reverse #used for namespaces
 
 
@@ -30,7 +31,42 @@ class ProfileView(TemplateView):
     template_name = 'accounts/profile.html'
 
     def get(self, request):
-        args = {'user': request.user}
+
+        cursor = connection.cursor()
+        cursor.execute("SELECT DISTINCT     home_classes.class_code \
+                        FROM                accounts_enrolledin, \
+                                            home_classes \
+                        WHERE               home_classes.class_code = accounts_enrolledin.class_code AND \
+                                            accounts_enrolledin.netID = %s \
+                        ORDER BY            home_classes.class_code", [str(request.user)])
+
+        enrolled_classes_arr = cursor.fetchall()
+
+        enrolled_classes = []
+
+        for i in range(len(enrolled_classes_arr)):
+            enrolled_classes.append(enrolled_classes_arr[i][0])
+
+        #print(enrolled_classes)
+
+        cursor = connection.cursor()
+        cursor.execute("SELECT DISTINCT     home_classes.class_code \
+                        FROM                accounts_enrolledin, \
+                                            home_classes\
+                        WHERE               home_classes.class_code <> accounts_enrolledin.class_code AND \
+                                            accounts_enrolledin.netID = %s \
+                        ORDER BY            home_classes.class_code", [str(request.user)])
+
+        other_classses_arr = cursor.fetchall()
+
+        other_classes = []
+
+        for i in range(len(other_classses_arr)):
+            other_classes.append(other_classses_arr[i][0])
+
+        #print(other_classes)
+
+        args = {'user': request.user, 'enrolled_classes': enrolled_classes, 'other_classes': other_classes}
         return render(request, self.template_name, args)
 
     def post():
