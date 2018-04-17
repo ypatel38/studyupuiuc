3fa3e528be804e934d25a2792e94d1a334bf117e
jjmarsz2 <jjmarsz2@illinois.edu>
2018-04-16 20:12:11 -0500

    collected active session data in account's view.py

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse #used for namespaces
 from django.http import HttpResponse
 
 from accounts.forms import RegistrationForm
+from datetime import datetime
 
 
 
@@ -31,7 +32,6 @@ class ProfileView(TemplateView):
     template_name = 'accounts/profile.html'
 
     def get(self, request):
-
         cursor = connection.cursor()
         cursor.execute("SELECT DISTINCT     home_classes.class_code \
                         FROM                accounts_enrolledin, \
@@ -76,6 +76,60 @@ class ProfileView(TemplateView):
             other_classes.append(other_classses_arr[i][0])
 
         #print(other_classes)
+
+
+
+        #########################################################################################################
+        #       Snippet of code that obtains each location and the # of times it is actively being used         #
+        #                       will be pasted in corresponding HTML page when it is created                    #
+        #########################################################################################################
+
+        cursor.execute("SELECT home_studysession.building, home_studysession.date, home_studysession.start_time, home_studysession.end_time, home_studysession.seshID        \
+                        FROM home_studysession")
+        building_arr = cursor.fetchall()
+
+        #now that we have every study_session ever, i need to populate the return dictionary
+
+        building_dict = {}
+        today = datetime.now().date()
+        valid_sesh = []
+        curr_time = datetime.now()
+        for i in range(len(building_arr)):
+            #first test if session is on current date
+            if (today - building_arr[i][1]).days == 0:
+                #session is today
+                #check if start time is in the past
+                comb = datetime.combine(today, building_arr[i][2])
+                if curr_time  >= comb:
+                    #session has started
+                    #check if it is still active, end time in future
+                    comb = datetime.combine(today, building_arr[i][3])
+                    if comb > curr_time:
+                        #session is active, consider this tuple as a valid data point
+                        if building_arr[i][0] not in building_dict:
+                            building_dict[building_arr[i][0]] = {}
+                            building_dict[building_arr[i][0]]['num_sesh'] = 1
+                            building_dict[building_arr[i][0]]['num_students'] = 0
+                            valid_sesh.append(building_arr[i][4])
+                        else:
+                            building_dict[building_arr[i][0]] += 1
+
+        #now get number of students per a valid study session
+
+        for i in valid_sesh:
+            cursor.execute("SELECT  home_studysession.building, COUNT(home_sessionhas.netID)         \
+                            FROM    home_studysession, home_sessionhas        \
+                            WHERE   home_studysession.seshID = home_sessionhas.seshID AND      \
+                                    home_studysession.seshID = %s       \
+                            GROUP BY    home_studysession.building", [str(i)])
+            temp_arr = cursor.fetchall()
+            building_dict[temp_arr[0][0]]['num_students'] += temp_arr[0][1]
+
+
+        print("Currently active study sessions: ")
+        for i in building_dict.keys():
+            print("There are", building_dict[i]['num_sesh'], "study sessions in", i, "with", building_dict[i]['num_students'], "students.")
+
 
         args = {'user': request.user, 'enrolled_classes': enrolled_classes, 'other_classes': other_classes}
         return render(request, self.template_name, args)
